04c7a665964e17bab48e45154c886781f2a64cf7
Josh <josh@maffey.com>
2021-01-17 08:13:04 +1300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='PlexMovieAgentMapper',
-    version='0.1.0',
+    version='0.1.1',
     packages=['plexmovieagentmapper'],
     url='https://github.com/josh-gaby/plex-movie-agent-mapper',
     license='',
